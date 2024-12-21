27cc5a8378497144c8b652cd9806ad915a6292b0
Alperen <alperenmimarlar@gmail.com>
2024-12-21 14:37:20 +0300

    BreadType, Burger, BurgerDao and BurgerDaoImpl added

---
+++ src/main/java/com/workintech/s18d1/dao/BurgerDaoImpl.java
@@ -0,0 +1,11 @@
+package com.workintech.s18d1.dao;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+public class BurgerDaoImpl implements BurgerDao{
+
+}
